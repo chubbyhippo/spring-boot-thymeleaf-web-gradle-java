15467ed1be003a117cf4f09922e86a9777528b17
ch <mkmsacc@outlook.com>
2023-12-14 13:18:58 +0700

    Add test for RootController
    Added a new unit test for RootController in a Spring application to ensure it correctly handles root requests. The test verifies that a GET request to the root path returns the correct status and view.

---
+++ src/test/java/com/example/demo/presentation/RootControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.presentation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@WebMvcTest(controllers = RootController.class)
+class RootControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Should return index page response")
+    void shouldReturnIndexPageResponse() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
