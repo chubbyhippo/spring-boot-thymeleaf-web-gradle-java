495c6fc2a9a28603549e5b24082eca05198caa7c
ch <mkmsacc@outlook.com>
2023-12-16 10:36:26 +0700

    Add Node-Gradle plugin to build.gradle file
    The build.gradle.kts was updated to include the Node-Gradle plugin version "7.0.1". This addition allows projects to run Node.js scripts as part of the Gradle build, enabling further use of Node.js-related tasks in the project's build lifecycle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,45 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.0"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.github.node-gradle.node") version "7.0.1"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 sourceSets {
-	getByName("main") {
-		resources {
-			exclude("static/css/application.css")
-		}
-	}
+    getByName("main") {
+        resources {
+            exclude("static/css/application.css")
+        }
+    }
 }
 
