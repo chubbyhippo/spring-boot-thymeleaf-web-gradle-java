1f7fe9040d9bb726820b313e1936eead0c3365c1
ch <mkmsacc@outlook.com>
2023-12-17 20:10:34 +0700

    Update test method in DemoApplicationTests
    Renamed test method 'contextLoads()' to 'shouldRunMainWithoutException()' and added functionality to ensure that main function runs without throwing any exceptions. This includes defining a RANDOM_PORT_NUMBER and using AssertJ's 'assertThatNoException' to confirm no exceptions are thrown when 'DemoApplication.main' is called with the random port number.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
 @SpringBootTest
 class DemoApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void shouldRunMainWithoutException() {
+		final var RANDOM_PORT_NUMBER = "--server.port=0";
+		assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{RANDOM_PORT_NUMBER}));
 	}
 
 }
