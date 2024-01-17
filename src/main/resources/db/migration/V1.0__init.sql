2541b3b86d73e66c40d2f0989002a6d1ce1ccdaf
ch <mkmsacc@outlook.com>
2024-01-17 13:36:10 +0700

    Refactor 'user' table creation in SQL migration file
    Changed the structure of 'user' table creation query in the initial SQL migration file. Now, the 'id' field is explicitly set as the primary key within the field definition line, rather than at the end of the create table statement, aiming to improve clarity and readability of the code.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE user
 (
-    id           UUID    NOT NULL,
-    first_name   VARCHAR NOT NULL,
-    last_name    VARCHAR NOT NULL,
-    gender       VARCHAR NOT NULL,
-    birthday     DATE    NOT NULL,
-    email        VARCHAR NOT NULL,
-    phone_number VARCHAR NOT NULL,
-    PRIMARY KEY (id)
+    id           UUID PRIMARY KEY NOT NULL,
+    first_name   VARCHAR          NOT NULL,
+    last_name    VARCHAR          NOT NULL,
+    gender       VARCHAR          NOT NULL,
+    birthday     DATE             NOT NULL,
+    email        VARCHAR          NOT NULL,
+    phone_number VARCHAR          NOT NULL,
+
 );
