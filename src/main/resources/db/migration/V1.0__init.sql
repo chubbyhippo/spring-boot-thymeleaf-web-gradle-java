086cd078f146f5378405016397b4b9c3b7188dfe
ch <mkmsacc@outlook.com>
2024-01-18 15:04:30 +0700

    Add 'gender' enum and rename 'users' table to 'thymeleaf_user'
    Introduced a new type 'gender' as an ENUM with values ('MALE', 'FEMALE', 'OTHER', 'UNKNOWN'). Additionally, renamed 'users' table to 'thymeleaf_user' and changed 'gender' field type to newly created ENUM 'gender' for stronger data integrity.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,9 +1,10 @@
-CREATE TABLE users
+CREATE TYPE gender AS ENUM('MALE', 'FEMALE', 'OTHER', 'UNKNOWN');
+CREATE TABLE thymeleaf_user
 (
     id            UUID PRIMARY KEY NOT NULL,
     first_name    VARCHAR          NOT NULL,
     last_name     VARCHAR          NOT NULL,
-    gender        VARCHAR          NOT NULL,
+    gender        gender           NOT NULL,
     date_of_birth DATE             NOT NULL,
     email         VARCHAR          NOT NULL,
     phone_number  VARCHAR          NOT NULL
