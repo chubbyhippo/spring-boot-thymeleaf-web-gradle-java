5b344477ecc44c5bba5c592b865e7a0fa6bf0519
ch <mkmsacc@outlook.com>
2024-01-18 15:04:52 +0700

    Update User entity with additional fields
    Additional fields including the "Gender" enum, first name, last name, date of birth, email, and phone number are now included in the User entity. Also, the name of the

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,14 +1,12 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -16,11 +14,22 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @Setter
-@Table(name = "user")
+@Table(name = "thymeleaf_user")
 public class User {
     @Id
     @GeneratedValue
     private UUID id;
-
+    @Column(name = "first_name")
+    private String firstname;
+    @Column(name = "last_name")
+    private String lastname;
+    @Column(name = "gender")
+    private Gender gender;
+    @Column(name = "date_of_birth")
+    private LocalDate dob;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
 }
