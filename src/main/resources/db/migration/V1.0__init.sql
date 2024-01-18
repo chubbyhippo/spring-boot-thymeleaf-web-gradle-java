c8703065044a849e016fee70697a2c50455760c0
ch <mkmsacc@outlook.com>
2024-01-18 13:33:20 +0700

    The provided commit message does not match with the diff provided. Based on the given diff, a correct commit message would be:
    Change 'birthday' field to 'date_of_birth' in users table
    
    The 'birthday' field in the 'users' table has been renamed to 'date_of_birth' to improve clarity. This change makes the purpose of the field more explicit to anybody viewing or managing the table in the database.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
-    id           UUID PRIMARY KEY NOT NULL,
-    first_name   VARCHAR          NOT NULL,
-    last_name    VARCHAR          NOT NULL,
-    gender       VARCHAR          NOT NULL,
-    birthday     DATE             NOT NULL,
-    email        VARCHAR          NOT NULL,
-    phone_number VARCHAR          NOT NULL
+    id            UUID PRIMARY KEY NOT NULL,
+    first_name    VARCHAR          NOT NULL,
+    last_name     VARCHAR          NOT NULL,
+    gender        VARCHAR          NOT NULL,
+    date_of_birth DATE             NOT NULL,
+    email         VARCHAR          NOT NULL,
+    phone_number  VARCHAR          NOT NULL
 );
