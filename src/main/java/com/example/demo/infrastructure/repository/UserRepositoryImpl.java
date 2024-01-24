5d692938b55b7d37676f62c8917ecb4d7ca6e6b2
ch <mkmsacc@outlook.com>
2024-01-24 15:24:13 +0700

    Refactor UserRepositoryImpl and enhance User management
    Refactored UserRepositoryImpl code by renaming the save method to createUser for clarity. Additionally, implemented the getUsers method to allow fetching of User entities in pages for more efficient data access.

--- src/main/java/com/example/demo/infrastructure/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/infrastructure/repository/UserRepositoryImpl.java
@@ -3,14 +3,24 @@ package com.example.demo.infrastructure.repository;
 import com.example.demo.domain.model.User;
 import com.example.demo.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
     private final UserJpaRepository repository;
 
     @Override
-    public User save(User user) {
+    public User createUser(User user) {
         return repository.save(user);
     }
+
+    @Override
+    public Page<User> getUsers(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
 }
