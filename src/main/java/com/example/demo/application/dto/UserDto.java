5e4e19ac0e800eba2f2bba107f95e531d7aabb16
ch <mkmsacc@outlook.com>
2024-01-24 15:23:32 +0700

    Add new UserDto class
    A new UserDto class has been created in the dto directory of the application. This file includes user detail attributes like name, gender, birthday, and phone number, which will be used when mapping raw user data.

---
+++ src/main/java/com/example/demo/application/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.application.dto;
+
+import com.example.demo.domain.model.Gender;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public record UserDto(
+        String name,
+        Gender gender,
+        LocalDate birthday,
+        String phoneNumber
+) {
+}
