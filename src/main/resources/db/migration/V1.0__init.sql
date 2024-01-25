a4038fce93ce8558d911c7ac77bd587f343ee42d
ch <mkmsacc@outlook.com>
2024-01-25 10:36:28 +0700

    Replace 'gender' enum with varchar in thymeleaf_user table
    The 'gender' enum type has been removed from the database schema and replaced with a VARCHAR type in the thymeleaf_user table. This change provides more flexibility for the 'gender' field, allowing a wider range of input data.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,10 +1,9 @@
-CREATE TYPE gender AS ENUM('MALE', 'FEMALE', 'OTHER', 'UNKNOWN');
 CREATE TABLE thymeleaf_user
 (
     id            UUID PRIMARY KEY NOT NULL,
     first_name    VARCHAR          NOT NULL,
     last_name     VARCHAR          NOT NULL,
-    gender        gender           NOT NULL,
+    gender        VARCHAR          NOT NULL,
     date_of_birth DATE             NOT NULL,
     email         VARCHAR          NOT NULL,
     phone_number  VARCHAR          NOT NULL
