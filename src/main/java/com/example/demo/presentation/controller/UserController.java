1a4b37b2ad2c7e2e2f9915d6b55b9b5bc81055bb
ch <mkmsacc@outlook.com>
2024-02-07 16:03:57 +0700

    Add createUserForm method in UserController
    Added a new method called createUserForm in UserController. This method is responsible for handling GET requests at "/users/create" and initializing the model with a CreateUserDto object and a list of Gender values. It will be used to display the form for creating a new user.

--- src/main/java/com/example/demo/presentation/controller/UserController.java
+++ src/main/java/com/example/demo/presentation/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.demo.presentation.controller;
 
+import com.example.demo.application.dto.CreateUserDto;
 import com.example.demo.application.service.UserService;
+import com.example.demo.domain.model.Gender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.SortDefault;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -23,5 +27,12 @@ public class UserController {
                         Pageable pageable) {
         model.addAttribute("users", service.getUsers(pageable));
         return "users/list";
+    }
+
+    @GetMapping("/create")
+    public String createUserForm(Model model) { //<.>
+        model.addAttribute("user", CreateUserDto.builder().build());
+        model.addAttribute("genders", List.of(Gender.MALE, Gender.FEMALE, Gender.OTHER, Gender.UNKNOWN));
+        return "users/edit";
     }
 }
