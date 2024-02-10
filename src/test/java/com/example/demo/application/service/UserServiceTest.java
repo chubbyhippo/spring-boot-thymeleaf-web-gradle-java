2127ff9e245a1e7fefd7688d3e9f85f65fa78796
ch <mkmsacc@outlook.com>
2024-02-10 15:41:32 +0700

    Refactor UserServiceTest to utilize shared TestUtil
    The UserServiceTest has been revised to use the shared TestUtil class instead of creating test users manually. This change not only standardises the process of generating test users, but also reduces code duplication across the test suite.

--- src/test/java/com/example/demo/application/service/UserServiceTest.java
+++ src/test/java/com/example/demo/application/service/UserServiceTest.java
@@ -1,10 +1,8 @@
 package com.example.demo.application.service;
 
 import com.example.demo.application.mapper.UserMapper;
-import com.example.demo.domain.model.Gender;
-import com.example.demo.domain.model.User;
 import com.example.demo.domain.repository.UserRepository;
-import net.datafaker.Faker;
+import com.example.demo.shared.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
@@ -38,23 +31,10 @@ class UserServiceTest {
     @Test
     @DisplayName("should get users")
     void shouldGetUsers() {
-
-        var faker = new Faker();
-        var genders = Gender.values();
-        var randomGender = genders[ThreadLocalRandom.current().nextInt(genders.length)];
-        var user = User.builder()
-                .id(UUID.randomUUID())
-                .firstname(faker.elderScrolls().firstName())
-                .lastname(faker.elderScrolls().lastName())
-                .gender(randomGender)
-                .email(faker.internet().emailAddress())
-                .phoneNumber(String.valueOf(faker.phoneNumber()))
-                .build();
-
         when(userRepository.getUsers(any(Pageable.class)))
-                .thenReturn(new PageImpl<>(List.of(user)));
+                .thenReturn(TestUtil.createPageOfUsers());
         var result = userService.getUsers(PageRequest.of(0, 2));
 
-        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).isNotNull();
     }
 }
