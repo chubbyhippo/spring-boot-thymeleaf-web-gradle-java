c595150713afa2ea3f44c3c3b6b99517bdeccda4
ch <mkmsacc@outlook.com>
2024-02-10 14:31:16 +0700

    Fix test case in DemoApplicationTests
    Updated the test case 'shouldRunMainWithoutException' by replacing 'DemoApplication.main()' with 'TestDemoApplication.main()'. The fix was necessary to correctly reference the correct application context in the ApplicationContextHolder.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -12,7 +12,7 @@ class DemoApplicationTests {
 	@Test
 	void shouldRunMainWithoutException() {
 		final var RANDOM_PORT_NUMBER = "--server.port=0";
-		assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{RANDOM_PORT_NUMBER}));
+		assertThatNoException().isThrownBy(() -> TestDemoApplication.main(new String[]{RANDOM_PORT_NUMBER}));
 	}
 
 }
