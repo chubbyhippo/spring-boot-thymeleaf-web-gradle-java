ec1d49b10dcf1b7a707b3f5ae70eeb6f3cf8a00d
ch <mkmsacc@outlook.com>
2024-02-10 15:41:23 +0700

    Refactor UserMapperTest to utilize shared TestUtil
    The UserMapperTest method now employs the shared TestUtil method in creating test users, replacing the prior manual creation process. This modification enhances test uniformity and minimizes code duplication within the testing suite.

--- src/test/java/com/example/demo/application/mapper/UserMapperTest.java
+++ src/test/java/com/example/demo/application/mapper/UserMapperTest.java
@@ -1,16 +1,11 @@
 package com.example.demo.application.mapper;
 
-import com.example.demo.domain.model.Gender;
-import com.example.demo.domain.model.User;
-import net.datafaker.Faker;
+import com.example.demo.shared.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,16 +18,7 @@ class UserMapperTest {
     @Test
     @DisplayName("should return response user dto")
     void shouldReturnResponseUserDto() {
-        var faker = new Faker();
-        var user = User.builder()
-                .id(UUID.randomUUID())
-                .firstname(faker.elderScrolls().firstName())
-                .lastname(faker.elderScrolls().lastName())
-                .gender(Gender.values()[ThreadLocalRandom.current().nextInt(Gender.values().length)])
-                .dob(faker.date().birthdayLocalDate())
-                .email(faker.internet().emailAddress())
-                .phoneNumber(faker.phoneNumber().toString())
-                .build();
+        var user = TestUtil.createUser();
 
         var responseUserDto = userMapper.toResponseUserDto(user);
         assertEquals(user.getFirstname() + " " + user.getLastname(), responseUserDto.name());
