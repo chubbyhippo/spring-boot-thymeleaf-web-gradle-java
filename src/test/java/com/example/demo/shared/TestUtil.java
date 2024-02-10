592b439b95c09484d21074ae252cd522b4980fa6
ch <mkmsacc@outlook.com>
2024-02-10 15:40:47 +0700

    Add TestUtil class for shared test functionality
    Introduced TestUtil.java under 'src/test/java/com/example/demo/shared' for shared test helper methods. The class includes methods for creating fake User objects, pages of User objects, and pages of ResponseUserDto. These methods will simplify and standardize test setup processes across different test classes.

---
+++ src/test/java/com/example/demo/shared/TestUtil.java
@@ -0,0 +1,42 @@
+package com.example.demo.shared;
+
+import com.example.demo.application.dto.ResponseUserDto;
+import com.example.demo.application.mapper.UserMapper;
+import com.example.demo.domain.model.Gender;
+import com.example.demo.domain.model.User;
+import net.datafaker.Faker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+public class TestUtil {
+
+    private static final Faker faker = new Faker();
+
+    public static User createUser() {
+        return User.builder()
+                .id(UUID.randomUUID())
+                .firstname(faker.elderScrolls().firstName())
+                .lastname(faker.elderScrolls().lastName())
+                .gender(Gender.values()[ThreadLocalRandom.current().nextInt(Gender.values().length)])
+                .email(faker.internet().emailAddress())
+                .phoneNumber(String.valueOf(faker.phoneNumber()))
+                .build();
+
+    }
+
+    public static Page<User> createPageOfUsers() {
+        return new PageImpl<>(IntStream.range(0, 10)
+                .mapToObj(operand -> createUser())
+                .toList());
+    }
+
+    public static Page<ResponseUserDto> createPageOfResponseDtos() {
+        var UserMapper = new UserMapper();
+        return createPageOfUsers().map(UserMapper::toResponseUserDto);
+    }
+
+}
