be94aa2a8e121821d1211f8e7cf1afe4a6951e54
ch <mkmsacc@outlook.com>
2024-02-10 15:40:55 +0700

    Update DataLoader class to use TestUtil
    Refactored DataLoader.java to use TestUtil for user creation. This new approach will allow better consistency in creating fake user data for test purposes, replacing the previously scattered functionalities with centralized, reusable methods in the TestUtil class.

--- src/test/java/com/example/demo/infrastructure/config/DataLoader.java
+++ src/test/java/com/example/demo/infrastructure/config/DataLoader.java
@@ -1,16 +1,13 @@
 package com.example.demo.infrastructure.config;
 
-import com.example.demo.domain.model.Gender;
-import com.example.demo.domain.model.User;
 import com.example.demo.domain.repository.UserRepository;
-import net.datafaker.Faker;
+import com.example.demo.shared.TestUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 @Configuration
@@ -29,16 +26,9 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        log.info("JDBC URL : {}" , jdbcUrl);
+        log.info("JDBC URL : {}", jdbcUrl);
         IntStream.range(0, 100)
-                .mapToObj(i -> new Faker()).map(faker -> User.builder()
-                        .firstname(faker.elderScrolls().firstName())
-                        .lastname(faker.touhou().characterLastName())
-                        .dob(faker.date().birthdayLocalDate())
-                        .email(faker.internet().emailAddress())
-                        .gender(Gender.values()[ThreadLocalRandom.current().nextInt(Gender.values().length)])
-                        .phoneNumber(faker.phoneNumber().phoneNumber())
-                        .build())
+                .mapToObj(i -> TestUtil.createUser())
                 .forEach(userRepository::createUser);
 
     }
