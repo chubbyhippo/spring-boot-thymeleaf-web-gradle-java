a5a58a39e452e9f67392c9adf750b638186ecad7
ch <mkmsacc@outlook.com>
2024-02-10 15:41:17 +0700

    Update UserJpaRepositoryTest to use shared TestUtil
    Removed manual user creation and refactored the UserJpaRepositoryTest to use shared TestUtil for creating test users. This change streamlines test user creation, ensuring consistency across tests and reducing redundancy in the testing code.

--- src/test/java/com/example/demo/infrastructure/UserJpaRepositoryTest.java
+++ src/test/java/com/example/demo/infrastructure/UserJpaRepositoryTest.java
@@ -1,18 +1,14 @@
 package com.example.demo.infrastructure;
 
 import com.example.demo.AbstractContainersTest;
-import com.example.demo.domain.model.Gender;
-import com.example.demo.domain.model.User;
 import com.example.demo.infrastructure.repository.UserJpaRepository;
-import net.datafaker.Faker;
+import com.example.demo.shared.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,18 +23,9 @@ class UserJpaRepositoryTest {
     @Test
     @DisplayName("should save user")
     void shouldSaveUser() {
-        var faker = new Faker();
-        var genders = Gender.values();
-        var randomGender = genders[ThreadLocalRandom.current().nextInt(genders.length)];
-        var savedUser = repository.save(User.builder()
-                .firstname(faker.elderScrolls().firstName())
-                .lastname(faker.elderScrolls().lastName())
-                .gender(randomGender)
-                .email(faker.internet().emailAddress())
-                .phoneNumber(String.valueOf(faker.phoneNumber()))
-                .build());
-
+        var savedUser = repository.save(TestUtil.createUser());
         var id = savedUser.getId();
+
         assertThat(id).isNotNull();
     }
 }
