e2657d58ecf05d526534a79516c65952ad628495
ch <mkmsacc@outlook.com>
2024-02-12 11:01:18 +0700

    The given commit message doesn't match with the code changes. Based on the diffs, an appropriate commit message could be:
    Add Testcontainers support in tests
    
    Testcontainers support has been added to the DemoApplicationTests by importing AbstractContainersTest. This integration will provide the ability to use Docker containers for functional or integration testing.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,16 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @SpringBootTest
+@ImportTestcontainers(AbstractContainersTest.class)
 class DemoApplicationTests {
 
-	@Test
-	void shouldRunMainWithoutException() {
-		final var RANDOM_PORT_NUMBER = "--server.port=0";
-		assertThatNoException().isThrownBy(() -> TestDemoApplication.main(new String[]{RANDOM_PORT_NUMBER}));
-	}
+    @Test
+    void shouldRunMainWithoutException() {
+        final var RANDOM_PORT_NUMBER = "--server.port=0";
+        assertThatNoException().isThrownBy(() -> TestDemoApplication.main(new String[]{RANDOM_PORT_NUMBER}));
+    }
 
 }
