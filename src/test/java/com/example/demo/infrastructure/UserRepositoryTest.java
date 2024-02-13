0669d33e29b1c6bb657a00b5fd1b1f4a3b834f71
ch <mkmsacc@outlook.com>
2024-02-13 12:21:18 +0700

    Rename and refactor UserJpaRepositoryTest to UserRepositoryTest
    The file UserJpaRepositoryTest has been renamed to UserRepositoryTest. In addition, the test has been updated to use Mockito for mocking dependencies, and now includes a new test case for the method "getUsers". The import statement has also been updated, replacing the use of TestUtil with TestUtils for better naming consistency.

---
+++ src/test/java/com/example/demo/infrastructure/UserRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.demo.infrastructure;
+
+import com.example.demo.AbstractContainersTest;
+import com.example.demo.domain.model.User;
+import com.example.demo.infrastructure.repository.UserJpaRepository;
+import com.example.demo.infrastructure.repository.UserRepositoryImpl;
+import com.example.demo.shared.TestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ImportTestcontainers(AbstractContainersTest.class)
+class UserRepositoryTest {
+
+    @Mock
+    private UserJpaRepository userJpaRepository;
+
+    @InjectMocks
+    private UserRepositoryImpl userRepository;
+
+    @Test
+    @DisplayName("should save user")
+    void shouldSaveUser() {
+
+        when(userJpaRepository.save(any(User.class))).thenReturn(TestUtils.createUser());
+        var savedUser = userRepository.createUser(TestUtils.createUser());
+        var id = savedUser.getId();
+
+        assertThat(id).isNotNull();
+    }
+
+    @Test
+    @DisplayName("should get page of users")
+    void shouldGetPageOfUsers() {
+        when(userJpaRepository.findAll(any(Pageable.class))).thenReturn(TestUtils.createPageOfUsers());
+        var pageRequest = PageRequest.of(0, 2);
+        var users = userRepository.getUsers(pageRequest);
+
+        assertThat(users.getTotalElements()).isPositive();
+    }
+}
