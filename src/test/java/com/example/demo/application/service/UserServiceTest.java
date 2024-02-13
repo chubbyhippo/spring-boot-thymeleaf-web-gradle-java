724b9e47155949af1e82a4e9862698cfe60cefe8
ch <mkmsacc@outlook.com>
2024-02-13 11:40:08 +0700

    Refactor TestUtil to TestUtils in UserServiceTest
    The import and usage of TestUtil in UserServiceTest has been replaced with TestUtils to maintain code consistency. The use of the more accurately named TestUtils aligns with naming conventions in the rest of the project.

--- src/test/java/com/example/demo/application/service/UserServiceTest.java
+++ src/test/java/com/example/demo/application/service/UserServiceTest.java
@@ -2,7 +2,7 @@ package com.example.demo.application.service;
 
 import com.example.demo.application.mapper.UserMapper;
 import com.example.demo.domain.repository.UserRepository;
-import com.example.demo.shared.TestUtil;
+import com.example.demo.shared.TestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,7 +32,7 @@ class UserServiceTest {
     @DisplayName("should get users")
     void shouldGetUsers() {
         when(userRepository.getUsers(any(Pageable.class)))
-                .thenReturn(TestUtil.createPageOfUsers());
+                .thenReturn(TestUtils.createPageOfUsers());
         var result = userService.getUsers(PageRequest.of(0, 2));
 
         assertThat(result.getContent()).isNotNull();
