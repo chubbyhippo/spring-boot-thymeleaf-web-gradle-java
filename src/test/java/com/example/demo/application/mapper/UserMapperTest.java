c547d403455f31e8bbc18eff89f3c9189d8b7baa
ch <mkmsacc@outlook.com>
2024-02-19 13:23:47 +0700

    Add new test case in UserMapperTest
    A new test case has been added to UserMapperTest to validate the creation of a User object from CreateUserDto. This check ensures that all fields from CreateUserDto are mapped correctly into User object. It also includes the import of additional assertion libraries to enhance test clarity.

--- src/test/java/com/example/demo/application/mapper/UserMapperTest.java
+++ src/test/java/com/example/demo/application/mapper/UserMapperTest.java
@@ -1,12 +1,14 @@
 package com.example.demo.application.mapper;
 
 import com.example.demo.shared.TestUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +28,21 @@ class UserMapperTest {
         assertEquals(user.getDob(), responseUserDto.birthday());
         assertEquals(user.getPhoneNumber(), responseUserDto.phoneNumber());
         assertEquals(user.getEmail(), responseUserDto.email());
+    }
+
+    @Test
+    @DisplayName("should return user from create user dto")
+    void shouldReturnUserFromCreateUserDto() {
+        var createUserDto = TestUtils.createCreateUserDto();
+        var user = userMapper.toUser(createUserDto);
+
+        assertThat(user.getFirstname()).isEqualTo(createUserDto.firstname());
+        assertThat(user.getLastname()).isEqualTo(createUserDto.lastname());
+        assertThat(user.getGender()).isEqualTo(createUserDto.gender());
+        assertThat(user.getEmail()).isEqualTo(createUserDto.email());
+        assertThat(user.getDob()).isEqualTo(createUserDto.birthday());
+        assertThat(user.getPhoneNumber()).isEqualTo(createUserDto.phoneNumber());
+
     }
 
 }
