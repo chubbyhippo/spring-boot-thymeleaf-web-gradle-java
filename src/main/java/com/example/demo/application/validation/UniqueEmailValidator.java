8abdb3ad9067264ed15d9a7c17e4e0d9f87caaf8
ch <mkmsacc@outlook.com>
2024-02-20 10:56:03 +0700

    Add UniqueEmailValidator for email uniqueness
    Implemented UniqueEmailValidator in the validation package. Now, the system verifies if an email is unique before it's used to create a new user. This helps prevent duplicate email addresses in the user repository.

---
+++ src/main/java/com/example/demo/application/validation/UniqueEmailValidator.java
@@ -0,0 +1,22 @@
+package com.example.demo.application.validation;
+
+import com.example.demo.domain.repository.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return !userRepository.existsByEmail(email);
+    }
+}
