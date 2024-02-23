566681eb64cc80f642a4f97d149fc96edca85869
ch <mkmsacc@outlook.com>
2024-02-23 23:15:07 +0700

    Add "uuid-ossp" extension and set default UUID generator
    The "uuid-ossp" extension has been added to the SQL migration file. A default UUID value is now generated by the "uuid_generate_v4()" method for the 'id' column in the 'thymeleaf_user' table.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,10 +1,13 @@
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE thymeleaf_user
 (
-    id            UUID PRIMARY KEY NOT NULL,
-    first_name    VARCHAR          NOT NULL,
-    last_name     VARCHAR          NOT NULL,
-    gender        VARCHAR          NOT NULL,
-    date_of_birth DATE             NOT NULL,
-    email         VARCHAR          NOT NULL,
-    phone_number  VARCHAR          NOT NULL
+    id            UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    first_name    VARCHAR                                     NOT NULL,
+    last_name     VARCHAR                                     NOT NULL,
+    gender        VARCHAR                                     NOT NULL,
+    date_of_birth DATE                                        NOT NULL,
+    email         VARCHAR                                     NOT NULL,
+    phone_number  VARCHAR                                     NOT NULL
 );
