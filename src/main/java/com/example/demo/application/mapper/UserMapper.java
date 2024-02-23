75fc63bcb8ed1108bed1eafb46df7d9d240d1b72
ch <mkmsacc@outlook.com>
2024-02-23 22:06:55 +0700

    Update UserMapper for User record usage
    The UserMapper class was updated to correspond with changes in the User class. The User class was transformed from a normal Java class to a record which resulted in changes to how its attributes are accessed. The changes in the UserMapper class now reflect the usage of methods to access these attributes rather than directly calling them.

--- src/main/java/com/example/demo/application/mapper/UserMapper.java
+++ src/main/java/com/example/demo/application/mapper/UserMapper.java
@@ -10,11 +10,11 @@ public class UserMapper {
 
     public ResponseUserDto toResponseUserDto(User user) {
         return ResponseUserDto.builder()
-                .name(user.getFirstname() + " " + user.getLastname())
-                .gender(user.getGender())
-                .birthday(user.getDob())
-                .phoneNumber(user.getPhoneNumber())
-                .email(user.getEmail())
+                .name(user.firstname() + " " + user.lastname())
+                .gender(user.gender())
+                .birthday(user.dob())
+                .phoneNumber(user.phoneNumber())
+                .email(user.email())
                 .build();
     }
 
