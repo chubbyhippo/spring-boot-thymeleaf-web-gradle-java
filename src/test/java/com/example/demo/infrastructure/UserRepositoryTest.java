8d42d7abc524b20192da42a280b3c3d59bcde99c
ch <mkmsacc@outlook.com>
2024-02-23 22:07:35 +0700

    Update UserRepositoryTest, using UserJdbcRepository instead of UserJpaRepository
    The UserRepositoryTest class now utilizes the UserJdbcRepository class for its operations rather than the UserJpaRepository class previously used. Negligible changes were also made to some methods to comply with this transition.

--- src/test/java/com/example/demo/infrastructure/UserRepositoryTest.java
+++ src/test/java/com/example/demo/infrastructure/UserRepositoryTest.java
@@ -2,15 +2,15 @@ package com.example.demo.infrastructure;
 
 import com.example.demo.AbstractContainersTest;
 import com.example.demo.domain.model.User;
-import com.example.demo.infrastructure.repository.UserJpaRepository;
+import com.example.demo.infrastructure.repository.UserJdbcRepository;
 import com.example.demo.infrastructure.repository.UserRepositoryImpl;
 import com.example.demo.shared.TestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,13 +19,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
-@DataJpaTest
+@DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ImportTestcontainers(AbstractContainersTest.class)
 class UserRepositoryTest {
 
     @Mock
-    private UserJpaRepository userJpaRepository;
+    private UserJdbcRepository userJdbcRepository;
 
     @InjectMocks
     private UserRepositoryImpl userRepository;
@@ -34,9 +34,9 @@ class UserRepositoryTest {
     @DisplayName("should save user")
     void shouldSaveUser() {
 
-        when(userJpaRepository.save(any(User.class))).thenReturn(TestUtils.createUser());
+        when(userJdbcRepository.save(any(User.class))).thenReturn(TestUtils.createUser());
         var savedUser = userRepository.createUser(TestUtils.createUser());
-        var id = savedUser.getId();
+        var id = savedUser.id();
 
         assertThat(id).isNotNull();
     }
@@ -44,7 +44,7 @@ class UserRepositoryTest {
     @Test
     @DisplayName("should get page of users")
     void shouldGetPageOfUsers() {
-        when(userJpaRepository.findAll(any(Pageable.class))).thenReturn(TestUtils.createPageOfUsers());
+        when(userJdbcRepository.findAll(any(Pageable.class))).thenReturn(TestUtils.createPageOfUsers());
         var pageRequest = PageRequest.of(0, 2);
         var users = userRepository.getUsers(pageRequest);
 
@@ -54,8 +54,8 @@ class UserRepositoryTest {
     @Test
     @DisplayName("should get existing state of email")
     void shouldGetExistingStateOfEmail() {
-        when(userJpaRepository.existsByEmail(any(String.class))).thenReturn(true);
-        var email = TestUtils.createUser().getEmail();
+        when(userJdbcRepository.existsByEmail(any(String.class))).thenReturn(true);
+        var email = TestUtils.createUser().email();
         assertThat(userRepository.existsByEmail(email)).isTrue();
     }
 }
