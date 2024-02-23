03632823d19b2fb9dfc3106f1415a0f78a10df9f
ch <mkmsacc@outlook.com>
2024-02-23 22:06:26 +0700

    Transform User entity class to record
    The User class was converted from a standard Java class to a Java record. As part of this transformation, JPA annotations were replaced with corresponding Spring Data annotations. This simplifies the code by removing boilerplate and eases its integration with Spring Data JDBC Repositories for better database interactions.

--- src/main/java/com/example/demo/domain/model/User.java
+++ src/main/java/com/example/demo/domain/model/User.java
@@ -1,36 +1,29 @@
 package com.example.demo.domain.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-@AllArgsConstructor
 @Builder
-@Entity
-@Getter
-@NoArgsConstructor
 @Table(name = "thymeleaf_user")
-public class User {
-    @Id
-    @GeneratedValue
-    private UUID id;
-    @Column(name = "first_name")
-    private String firstname;
-    @Column(name = "last_name")
-    private String lastname;
-    @Column(name = "gender")
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-    @Column(name = "date_of_birth")
-    private LocalDate dob;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "phone_number")
-    private String phoneNumber;
-
+public record User(
+        @Id
+        UUID id,
+        @Column("first_name")
+        String firstname,
+        @Column("last_name")
+        String lastname,
+        @Column("gender")
+        Gender gender,
+        @Column("date_of_birth")
+        LocalDate dob,
+        @Column("email")
+        String email,
+        @Column("phone_number")
+        String phoneNumber
+) {
 }
