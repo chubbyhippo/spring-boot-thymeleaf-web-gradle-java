09bf4f6b42f562fcf2a0f933dcace644d12a3736
ch <mkmsacc@outlook.com>
2024-02-25 14:39:50 +0700

    Add new fields to thymeleaf_user table in V1.0__init.sql
    The thymeleaf_user table has been updated with 'created_date', 'last_modified_date', and 'version' fields. These additional fields will help track changes to user data over time, and manage data versioning.

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -3,11 +3,14 @@ EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE thymeleaf_user
 (
-    id            UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    first_name    VARCHAR                                     NOT NULL,
-    last_name     VARCHAR                                     NOT NULL,
-    gender        VARCHAR                                     NOT NULL,
-    date_of_birth DATE                                        NOT NULL,
-    email         VARCHAR                                     NOT NULL,
-    phone_number  VARCHAR                                     NOT NULL
+    id                 UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    first_name         VARCHAR                                     NOT NULL,
+    last_name          VARCHAR                                     NOT NULL,
+    gender             VARCHAR                                     NOT NULL,
+    date_of_birth      DATE                                        NOT NULL,
+    email              VARCHAR                                     NOT NULL,
+    phone_number       VARCHAR                                     NOT NULL,
+    created_date       timestamp                                   NOT NULL,
+    last_modified_date timestamp                                   NOT NULL,
+    version            integer                                     NOT NULL
 );
