c63bcad423c1f50ed9deecb0378c0daadd4e7ee6
chubbyhippo <mkmsacc@outlook.com>
2024-03-31 17:11:43 +0700

    Remove unused Spring Boot test annotations in TestDemoApplication
    The TestDemoApplication class was refactored by removing springs boot test annotations - @TestConfiguration and @ImportTestcontainers. Now, the application will directly initialize using TestcontainersConfig class, thus simplifying the class and making it easier to maintain.

--- src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
+++ src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
@@ -1,15 +1,11 @@
 package io.github.chubbyhippo.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
-@TestConfiguration(proxyBeanMethods = false)
-@ImportTestcontainers(TestcontainersConfig.class)
 public class TestDemoApplication {
     public static void main(String[] args) {
         SpringApplication.from(DemoApplication::main)
-                .with(TestDemoApplication.class)
+                .with(TestcontainersConfig.class)
                 .run(args);
     }
 
