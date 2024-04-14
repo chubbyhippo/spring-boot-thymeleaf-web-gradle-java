07a285a827e3e1d732ab484afcc369c7461a6b0f
chubbyhippo <mkmsacc@outlook.com>
2024-04-14 12:22:21 +0700

    Remove Lombok's @RequiredArgsConstructor annotation
    A custom constructor has been added to UserRepositoryImpl replacing the Lombok's @RequiredArgsConstructor annotation. This change was made to explicitly define how the UserJdbcRepository dependency is being injected into UserRepositoryImpl.

--- src/main/java/io/github/chubbyhippo/mythyme/infrastructure/repository/UserRepositoryImpl.java
+++ src/main/java/io/github/chubbyhippo/mythyme/infrastructure/repository/UserRepositoryImpl.java
@@ -2,17 +2,19 @@ package io.github.chubbyhippo.mythyme.infrastructure.repository;
 
 import io.github.chubbyhippo.mythyme.domain.model.User;
 import io.github.chubbyhippo.mythyme.domain.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
     private final UserJdbcRepository repository;
+
+    public UserRepositoryImpl(UserJdbcRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     @Transactional
