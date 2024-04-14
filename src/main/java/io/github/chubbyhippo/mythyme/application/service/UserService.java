aed15c11f23f09b6032e64ecd4fc18e266d8389a
chubbyhippo <mkmsacc@outlook.com>
2024-04-14 12:22:27 +0700

    Replace Lombok annotation with constructor in UserService
    The Lombok `@RequiredArgsConstructor` annotation has been removed from the UserService class. Instead, a explicit constructor that initializes UserRepository and UserMapper has been added. This change improves readability and control over dependency injection.

--- src/main/java/io/github/chubbyhippo/mythyme/application/service/UserService.java
+++ src/main/java/io/github/chubbyhippo/mythyme/application/service/UserService.java
@@ -4,17 +4,20 @@ import io.github.chubbyhippo.mythyme.application.dto.RequestUserDto;
 import io.github.chubbyhippo.mythyme.application.dto.ResponseUserDto;
 import io.github.chubbyhippo.mythyme.application.mapper.UserMapper;
 import io.github.chubbyhippo.mythyme.domain.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository repository;
     private final UserMapper userMapper;
+
+    public UserService(UserRepository repository, UserMapper userMapper) {
+        this.repository = repository;
+        this.userMapper = userMapper;
+    }
 
     public Page<ResponseUserDto> getUsers(Pageable pageable) {
         return repository.getUsers(pageable)
