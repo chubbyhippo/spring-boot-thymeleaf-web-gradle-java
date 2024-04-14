400d55b091fe5fd532127fbbc8132625eedd959c
chubbyhippo <mkmsacc@outlook.com>
2024-04-14 10:54:45 +0700

    Implement custom builder for ResponseUserDto
    The Lombok @Builder annotation was removed from ResponseUserDto and replaced with a custom builder class. This allows for better control and flexibility when creating instances of ResponseUserDto. It contains methods for setting each individual property of the object.

--- src/main/java/io/github/chubbyhippo/mythyme/application/dto/ResponseUserDto.java
+++ src/main/java/io/github/chubbyhippo/mythyme/application/dto/ResponseUserDto.java
@@ -1,11 +1,9 @@
 package io.github.chubbyhippo.mythyme.application.dto;
 
 import io.github.chubbyhippo.mythyme.domain.model.Gender;
-import lombok.Builder;
 
 import java.time.LocalDate;
 
-@Builder
 public record ResponseUserDto(
         String name,
         Gender gender,
@@ -13,4 +11,48 @@ public record ResponseUserDto(
         String phoneNumber,
         String email
 ) {
+
+    public static final class ResponseUserDtoBuilder {
+        private String name;
+        private Gender gender;
+        private LocalDate birthday;
+        private String phoneNumber;
+        private String email;
+
+        private ResponseUserDtoBuilder() {
+        }
+
+        public static ResponseUserDtoBuilder aResponseUserDto() {
+            return new ResponseUserDtoBuilder();
+        }
+
+        public ResponseUserDtoBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ResponseUserDtoBuilder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public ResponseUserDtoBuilder withBirthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public ResponseUserDtoBuilder withPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public ResponseUserDtoBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public ResponseUserDto build() {
+            return new ResponseUserDto(name, gender, birthday, phoneNumber, email);
+        }
+    }
 }
