872fde9b660d75f4a186c9be3c6a17f92b6c0b1d
chubbyhippo <mkmsacc@outlook.com>
2024-04-14 11:00:06 +0700

    Replace Lombok builder with custom builder in RequestUserDto
    The Lombok builder dependency has been removed from RequestUserDto. Instead, we manually defined a custom RequestUserDtoBuilder inner class with setters for RequestUserDto's parameters. This change gives us better control over object creation and initialization.

--- src/main/java/io/github/chubbyhippo/mythyme/application/dto/RequestUserDto.java
+++ src/main/java/io/github/chubbyhippo/mythyme/application/dto/RequestUserDto.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Builder;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Builder
 public record RequestUserDto(
         UUID id,
         @NotBlank
@@ -33,4 +31,61 @@ public record RequestUserDto(
         String phoneNumber
 ) {
 
+    public static final class RequestUserDtoBuilder {
+        private UUID id;
+        private @NotBlank String firstname;
+        private @NotBlank String lastname;
+        private @NotNull Gender gender;
+        private @Email
+        @NotBlank String email;
+        private @NotNull LocalDate birthday;
+        private @NotBlank
+        @Pattern(regexp = "[0-9.\\-() x/+]+") String phoneNumber;
+
+        private RequestUserDtoBuilder() {
+        }
+
+        public static RequestUserDtoBuilder aRequestUserDto() {
+            return new RequestUserDtoBuilder();
+        }
+
+        public RequestUserDtoBuilder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withFirstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withLastname(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withBirthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public RequestUserDtoBuilder withPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public RequestUserDto build() {
+            return new RequestUserDto(id, firstname, lastname, gender, email, birthday, phoneNumber);
+        }
+    }
 }
