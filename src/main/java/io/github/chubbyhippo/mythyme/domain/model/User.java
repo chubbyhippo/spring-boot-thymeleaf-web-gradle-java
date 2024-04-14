05f50923f179c83be605f3cafecb4e8880160444
chubbyhippo <mkmsacc@outlook.com>
2024-04-14 12:17:19 +0700

    Implement custom UserBuilder in User
    The "@Builder" annotation from Lombok was removed. A custom UserBuilder has been implemented within User to replace the Lombok-generated builder. This custom builder follows the standard Builder pattern, and allows

--- src/main/java/io/github/chubbyhippo/mythyme/domain/model/User.java
+++ src/main/java/io/github/chubbyhippo/mythyme/domain/model/User.java
@@ -1,6 +1,5 @@
 package io.github.chubbyhippo.mythyme.domain.model;
 
-import lombok.Builder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,7 +11,6 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Builder
 @Table(name = "app_user")
 public record User(
         @Id
@@ -36,4 +34,78 @@ public record User(
         @Version
         int version
 ) {
+
+    public static final class UserBuilder {
+        private UUID id;
+        private String firstname;
+        private String lastname;
+        private Gender gender;
+        private LocalDate dob;
+        private String email;
+        private String phoneNumber;
+        private Instant createdDate;
+        private Instant lastModifiedDate;
+        private int version;
+
+        private UserBuilder() {
+        }
+
+        public static UserBuilder anUser() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder withFirstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public UserBuilder withLastname(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public UserBuilder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public UserBuilder withDob(LocalDate dob) {
+            this.dob = dob;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder withPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserBuilder withCreatedDate(Instant createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public UserBuilder withLastModifiedDate(Instant lastModifiedDate) {
+            this.lastModifiedDate = lastModifiedDate;
+            return this;
+        }
+
+        public UserBuilder withVersion(int version) {
+            this.version = version;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, firstname, lastname, gender, dob, email, phoneNumber, createdDate, lastModifiedDate, version);
+        }
+    }
 }
