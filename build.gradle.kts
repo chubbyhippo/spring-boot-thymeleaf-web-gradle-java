e0373bbcb31eaca15f51c99a3a4bb27d417a3255
chubbyhippo <mkmsacc@outlook.com>
2024-05-09 10:41:54 +0700

    Update Spring Boot version to 3.2.5
    In the build.gradle.kts file, the Spring Boot plugin version is upgraded from 3.2.4 to 3.2.5 for better functionality and improved performance.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.gradle.node.npm.task.NpxTask
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
+    id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     id("com.github.node-gradle.node") version "7.0.2"
 }
