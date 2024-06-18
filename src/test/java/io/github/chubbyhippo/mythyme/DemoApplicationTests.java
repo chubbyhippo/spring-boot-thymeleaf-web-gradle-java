f69912a8989217d9c808c05a0bc7be022b989564
ch <mkmsacc@outlook.com>
2024-06-18 21:42:16 +0700

    Add @Testcontainers annotation to DemoApplicationTests
    Added the @Testcontainers annotation to the DemoApplicationTests class. This change will ensure the tests will be executed in a Docker container. The 'disabledWithoutDocker' parameter is set to true, meaning tests will be disabled if

--- src/test/java/io/github/chubbyhippo/mythyme/DemoApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/mythyme/DemoApplicationTests.java
@@ -3,11 +3,13 @@ package io.github.chubbyhippo.mythyme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @SpringBootTest
 @Import(ContainersConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 class DemoApplicationTests {
 
     @Test
