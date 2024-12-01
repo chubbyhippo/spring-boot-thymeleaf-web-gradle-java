86d8d3b39922e949ea89c8f9b5e18a77bfc69d10
chubbyhippo <mkmsacc@outlook.com>
2024-12-01 20:56:16 +0700

    Update DataFaker to version 2.4.2
    This update bumps the DataFaker library from version 2.3.1 to 2.4.2 in the build configuration. The change ensures access to the latest features and bug fixes provided by the DataFaker library.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
     mavenCentral()
 }
 
-extra["datafakerVersion"] = "2.3.1"
+extra["datafakerVersion"] = "2.4.2"
 extra["alpinejsVersion"] = "3.14.1"
 extra["nodeVersion"] = "22.11.0"
 
